62e1ed28b67bb1253c01867f5a45ffb128d59d8f
Aleksei <alekseimistik@gmail.com>
2026-09-09 15:34:16 +0300

    for labs

--- src/lab01/ex2.py
+++ src/lab01/ex2.py
@@ -1,3 +1,7 @@
-a = int(input())
-b = int(input())
-print(f'sum={}; avg={}')
+a = input().replace(',', '.')
+b = input().replace(',', '.')
+print(a, b)
+a, b = float(a), float(b)
+summ = a + b
+avg = (a + b) / 2
+print(f'sum={summ:.2f}; avg={avg:.2f}')
