077b6c0f77c7a5dad4ba91515fe7aab5746739b4
Aleksei <alekseimistik@gmail.com>
2026-09-09 15:13:47 +0300

    for labs

--- src/lab01/ex1.py
+++ src/lab01/ex1.py
@@ -1,1 +1,6 @@
-print("Hello, world!")
+name = input()
+old = int(input())
+next_old = old + 1
+print(f"Имя: {name}")
+print(f"Возраст: {old}")
+print(f"Привет, Алиса! Через год тебе будет {next_old}.")
