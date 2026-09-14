54301ef90e2cca9e66be330f0f6814612255dd45
Aleksei <alekseimistik@gmail.com>
2026-09-14 21:23:42 +0300

    for labs

--- src/lab01/ex5.py
+++ src/lab01/ex5.py
@@ -2,7 +2,8 @@ name = input()
 ini = ''
 a = name.split()
 for i in a:
-    ini = ini + i[0] 
+    if i[0].isupper():
+        ini = ini + i[0] 
 ini = ini + '.'
 print(ini)
 print(len('+'.join(a)))
