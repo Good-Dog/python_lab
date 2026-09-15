20c970ef0c904ec291dff0c845761d76bd1d0eb7
Aleksei <alekseimistik@gmail.com>
2026-09-15 13:56:34 +0300

    for labs

--- src/lab01/ex5.py
+++ src/lab01/ex5.py
@@ -1,9 +1,9 @@
 name = input('ФИО: ')
 ini = ''
 a = name.split()
-for i in ''.join(a):
-    if i.isupper():
-        ini = ini + i
+for i in a:
+    if i[0].isupper():
+        ini = ini + i[0]
 ini = ini + '.'
 print(f'Инициалы: {ini}')
 print(f'Длина (символов): {len('+'.join(a))}')
